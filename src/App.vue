54995663615df8a3bda3e631177d1c023e92bad7
yusongH <549962989@qq.com>
2021-03-27 10:40:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <HelloWorld msg="Hello Vue 3 + Vite" /> -->
+    <div :class="$style.box"></div>
+</template>
+
+<script setup>
+// import HelloWorld from "./components/HelloWorld.vue";
+// console.log('==', $style);
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style module>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
+.box {
+    background-color: red;
+    width: 100px;
+    height: 100px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+    <h1>{{ msg }}</h1>
+
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank"
+            >Vite Documentation</a
+        >
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
+    </p>
+
+    <!-- <button @click="state.count++">count is: {{ state.count }}</button> -->
+    <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
+    <!-- <div :class="customStyle.box"></div> -->
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+// import style from "@/style/helloword.module.css";
+
+defineProps({
+    msg: String,
+});
+
+// const customStyle = computed(() => {
+//     return style;
+// });
+
+// const state = reactive({ count: 0 });
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+</style>
